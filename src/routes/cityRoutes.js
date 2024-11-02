206f2bb13ba7679ccaa3c164a13cc2031e4e0371
Dante Yucra <renatoyucra97@gmail.com>
2024-11-01 19:42:22 -0500

    update: middleware lawyerRoutes

--- src/routes/cityRoutes.js
+++ src/routes/cityRoutes.js
@@ -6,6 +6,8 @@ import {
     updateCity,
     deleteCity
 } from '../controllers/cityController.js';
+import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
+import { isTokenRevoked } from '../middlewares/isTokenRevoked.js';
 
 const router = express.Router();
 
@@ -13,10 +15,10 @@ router.get('/', getCities);
 
 router.get('/:id', getCityById);
 
-router.post('/', createCity);
+router.post('/', authenticate, isTokenRevoked, authorizeAdmin, createCity);
 
-router.put('/:id', updateCity);
+router.put('/:id', authenticate, isTokenRevoked, authorizeAdmin, updateCity);
 
-router.delete('/:id', deleteCity);
+router.delete('/:id', authenticate, isTokenRevoked, authorizeAdmin, deleteCity);
 
 export default router;
